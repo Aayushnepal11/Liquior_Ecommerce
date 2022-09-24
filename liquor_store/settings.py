a53a0f36e241163e78e81f2766dfefe170ca82a8
Aayushnepal11 <neymarnepal82@gmail.com>
2022-09-24 22:10:17 +0545

    HomePage

--- liquor_store/settings.py
+++ liquor_store/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #My_Core_Styling
+    # My_Core_Styling
     'django_bootstrap5',
-    #My_Apps
+    # Icon Fonts
+    
+    # My_Apps
     'shop',
 ]
 
