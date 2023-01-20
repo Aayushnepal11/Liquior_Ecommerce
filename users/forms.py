12a3dbe853531a8c4db49d0b3eeef8f13e416bfb
Aayushnepal11 <neymarnepal82@gmail.com>
2023-01-20 14:15:07 +0545

    Removing Glitches

--- users/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from . models import customerUser
-class RegistrationForm(forms.ModelForm):
-    class Meta:
-        fields = ['__all__']
-        model = customerUser
-        
