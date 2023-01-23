9bd1dc9b0a2f12cb96972980804eb20492d662aa
Aayushnepal11 <neymarnepal82@gmail.com>
2023-01-23 12:08:26 +0545

    Glitch Register

--- users/views.py
+++ users/views.py
@@ -1,22 +1,22 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import login
 from django.contrib.auth.forms  import UserCreationForm
-
+# from . form import UserCreationForm
+# from . models import CustomerUsers
 # Create your views here.
 def register(request):
-    """ Register a new user. """
-    if request.method != 'POST':
-        form = UserCreationForm()
-    else:
-        # Process completed form.
-        form = UserCreationForm(data=request.POST)
-
+    """ Creating the new user to be able to regsiter """
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             new_user = form.save()
-            # Log the user in and then redirect to home page.
             login(request, new_user)
-            return redirect('store:home')
-    # Display a blank or invalid form.
-    context = {'form': form}
-    return render(request, 'registration/register.html', context)
+            redirect('store:index')
+    else:
+        form = UserCreationForm()
+        context = {
+            'title': 'Register',
+            'form': form
+            }
+        return render(request, 'registration/register.html', context)
     
